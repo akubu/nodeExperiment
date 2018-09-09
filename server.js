1248a90306fce053b2b3c677395a78ac3fb334f8
akshay singh <akshaysingh@paisabazaar.com>
2018-09-09 15:46:41 +0530

    more changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -37,4 +38,4 @@ app.get('/about',(req,res)=>{
 });
 
 
-app.listen(3000);
+app.listen(port);
